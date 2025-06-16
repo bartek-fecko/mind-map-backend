eb9bb515bd6b9dd7474a6349a0273734460c9806
bfecko <bartlomiej.fecko@gmail.com>
2025-06-16 20:21:01 +0200

    commit #4

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -21,7 +21,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: 'lax',
+      sameSite: 'none',
       domain: frontendDomain,
     });
 
